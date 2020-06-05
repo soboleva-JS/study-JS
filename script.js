c483ab929bb5854ae690525349239cc2614fa061
Tatyana Soboleva <tatochka-a-a@yandex.ru>
2020-06-05 14:53:53 +0400

    дз2 обязательное

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 100000;
+let income = 'freelance';
+let addExpenses = 'Интернет, Такси, Коммуналка';
+let deposit = true;
+let mission = 5000000;
+let period = 12;
 
-alert ('Hello world!git');
-console.log('Hello world!');
+console.log('typeof money: ', typeof money);
+console.log('typeof income: ', typeof income);
+console.log('typeof deposit: ', typeof deposit);
+console.log('addExpenses lenght: ', addExpenses.length);
+console.log (`Период равен ${period} месяцев`);
+console.log (`Цель заработать ${mission} рублей`);
+console.log(addExpenses.toLowerCase().split(', '));
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
