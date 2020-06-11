ee90f06da3e031b2e2f10fceac81b0eb4b08537f
Tatyana Soboleva <66415940+soboleva-JS@users.noreply.github.com>
2020-06-11 14:54:55 +0400

    добавила рандом, поправила вызов функции

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 'use strict';
 
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max - min);
+  return Math.round(rand);
+};
+
 function isNumber (n) {
   return !isNaN(parseFloat(n)) ;
 };
 
 function one (n) {
-  return function two () {
+  function two () {
     let y = prompt ('Угадай число от 1 до 100');
     if (y === null) return;
     else if (!isNumber(y)) {
@@ -17,7 +22,7 @@ function one (n) {
             else {alert('Загаданное число больше'); return two ()};
       };
   };
+  return two();
 };
 
-const ask = one(50);
-ask ();
+one (randomInteger(1, 100));
