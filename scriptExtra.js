0debbf70d5ce0b6f3a9ba3c516f66d5e85977f82
Tatyana Soboleva <tatochka-a-a@yandex.ru>
2020-06-13 00:10:42 +0400

    дз7 доп

--- scriptExtra.js
+++ scriptExtra.js
@@ -1,50 +1,10 @@
 'use strict';
-let arr = [];
-let i = 0;
 
-function isNumber (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
+let arr = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+console.log(arr.join(', '));
+for (let item of arr) console.log (item);
+let str = arr[0] + ', '+arr[arr.length-1];
+console.log(str.italics());
 
-function zeroCut (n) {
-  let i=0;
-  while (n [i] === '0') i++;
-  return n.substr(i);
-  
-};
-
-function getArr () {
-do {
-  do arr [i] = zeroCut(prompt (`Введите многозначное ${i}-е число`,'  000001  ').trim());
-  while (!isNumber(arr [i] )||arr [i].length<2);
-  i++;
-}
-while (i<8)
-};
-
-function twoFourLog () {
-  for (let j = 0; j < arr.length; j ++) {
-    if (arr[j].trim().startsWith('2') || arr[j].trim().startsWith('4')) 
-    console.log(arr[j].trim());
-    }
-};
-
-getArr ();
-console.log(arr);
-twoFourLog ();
-
-let flag;
-function simpleNumbers () {
-  for (let n = 1; n < 99; n ++) {
-    flag = true;    
-    for (let k = 2; k < n; k ++) {
-      if ((n%k)===0) {
-        flag = false;
-        break;
-      };
-    };
-    if (flag) console.log(n,`Делители этого числа: 1 и ${n}`)
-  };
-};
-
-simpleNumbers();
+let currentDate = new Date();
+console.log(arr[currentDate.getDay()].bold());
