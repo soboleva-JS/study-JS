f9e3c14de468b8f4d8d9d5d60600363d07c41137
Tatyana Soboleva <tatochka-a-a@yandex.ru>
2020-06-18 10:08:48 +0400

    lp10 доп правки

--- scriptExtra.js
+++ scriptExtra.js
@@ -1,57 +1,58 @@
 'use strict';
 let arrDay = ['Воскресенье','Понедельник', 'Вторник' , 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-    arrMonth = ['января','февраля', 'марта' , 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    arrMonth = ['января','февраля', 'марта' , 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'],
+    zero = function (n) {
+    if (n<10) return '0'+n; 
+    else return n;
+    },
+    getDateLong = function (currentDate=new Date()) {
+      let yyyy,
+          day,
+          mm,
+          m,
+          dd,
+          hh,
+          MM,
+          ss,
+          str,
+          arrHour = ['час', 'часа', 'часов'],
+          hour;
+      yyyy =currentDate.getFullYear();
+      day = arrDay[currentDate.getDay()];
+      m = arrMonth[currentDate.getMonth()];
+      dd = (currentDate.getDate());
+      hh = (currentDate.getHours());
+      MM = (currentDate.getMinutes());
+      ss = (currentDate.getSeconds());
 
+      if (hh.toString().endsWith('1')) hour = arrHour[0]
+        else if ((parseInt(hh.toString().slice(-1)) > 1)&&(parseInt(hh.toString().slice(-1)) < 5)) hour = arrHour[1]
+        else hour = arrHour[2];
+      str = `Сегодня ${day}, ${dd} ${m} ${yyyy} года, ${hh} ${hour}, ${MM} минут, ${ss} секунды`;
+      document.body.innerHTML = '';
+      document.write(str,'<br>');
+    },
+    getDateShort = function (currentDate=new Date()) {
+      let yyyy,
+          day,
+          mm,
+          m,
+          dd,
+          hh,
+          MM,
+          ss,
+          str;
+      yyyy =currentDate.getFullYear();
+      day = arrDay[currentDate.getDay()];
+      mm = zero((currentDate.getMonth() + 1));
+      dd = zero(currentDate.getDate());
+      hh = zero(currentDate.getHours());
+      MM = zero(currentDate.getMinutes());
+      ss = zero(currentDate.getSeconds());
 
-let getDateLong = function (currentDate=new Date()) {
-  let yyyy,
-      day,
-      mm,
-      m,
-      dd,
-      hh,
-      MM,
-      ss,
-      str,
-      arrHour = ['час', 'часа', 'часов'],
-      hour;
-  yyyy =currentDate.getFullYear();
-  day = arrDay[currentDate.getDay()];
-  mm = (''+(currentDate.getMonth() + 1)).slice(-2);
-  m = arrMonth[currentDate.getMonth()];
-  dd = (''+currentDate.getDate()).slice(-2);
-  hh = (''+currentDate.getHours()).slice(-2);
-  MM = (''+currentDate.getMinutes()).slice(-2);
-  ss = (''+currentDate.getSeconds()).slice(-2);
-
-  if (hh.toString().endsWith('1')) hour = arrHour[0]
-    else if ((parseInt(hh.toString().slice(-1)) > 1)&&(parseInt(hh.toString().slice(-1)) < 5)) hour = arrHour[1]
-    else hour = arrHour[2];
-  str = `Сегодня ${day}, ${dd} ${m} ${yyyy} года, ${hh} ${hour}, ${MM} минут, ${ss} секунды`;
-  document.write(str,'<br>');
-};
-let getDateShort = function (currentDate=new Date()) {
-  let yyyy,
-      day,
-      mm,
-      m,
-      dd,
-      hh,
-      MM,
-      ss,
-      str;
-  yyyy =currentDate.getFullYear();
-  day = arrDay[currentDate.getDay()];
-  mm = ('0'+(currentDate.getMonth() + 1)).slice(-2);
-  m = arrMonth[currentDate.getMonth() + 1];
-  dd = ('0'+currentDate.getDate()).slice(-2);
-  hh = ('0'+currentDate.getHours()).slice(-2);
-  MM = ('0'+currentDate.getMinutes()).slice(-2);
-  ss = ('0'+currentDate.getSeconds()).slice(-2);
-
-  str = dd+'.'+mm+'.'+yyyy+ ' - '+ hh+'.'+MM+'.'+ss;
-  document.write(str,'<br>');
-};
+      str = dd+'.'+mm+'.'+yyyy+ ' - '+ hh+'.'+MM+'.'+ss;
+        document.write(str,'<br>');
+    };
 
 setInterval(getDateLong, 1000);
 setInterval(getDateShort, 1000);
