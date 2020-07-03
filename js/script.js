3056d7654d8fde63219fd9bd4c7ac9100cbc3ee6
Tatyana Soboleva <tatochka-a-a@yandex.ru>
2020-07-03 16:35:14 +0400

    дз22 доп

--- js/script.js
+++ js/script.js
@@ -20,19 +20,20 @@ class Todo {
     this.addToStorage();
   }
 
-  createItem = (todo) => {   
-      const li = document.createElement('li');
-      li.classList.add('todo-item');
-      li.key = todo.key;
-      li.insertAdjacentHTML('beforeend', `
+  createItem = (todo) => {
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+    li.key = todo.key;
+    li.insertAdjacentHTML('beforeend', `
       <span class="text-todo">${todo.value}</span>
       <div class="todo-buttons">
+        <button class="todo-edit"></button>
         <button class="todo-remove"></button>
         <button class="todo-complete"></button>
       </div>
       `);
-      if (todo.completed) this.todoCompleted.append(li)
-      else this.todoList.append(li);    
+    if (todo.completed) this.todoCompleted.append(li)
+    else this.todoList.append(li);
   }
 
   addTodo() {
@@ -41,38 +42,125 @@ class Todo {
       alert('Пустое дело добавить нельзя');
       return
     } else {
-    if (this.input.value.trim()) {
-      const newTodo = {
-        value: this.input.value,
-        completed: false,
-        key: this.generateKey(),
-      };
-      this.todoData.set(newTodo.key, newTodo);
-      console.log(...this.todoData);
-      this.render();
+      if (this.input.value.trim()) {
+        const newTodo = {
+          value: this.input.value,
+          completed: false,
+          key: this.generateKey(),
+        };
+        this.todoData.set(newTodo.key, newTodo);
+        this.render();
+      }
     }
   }
+
+  animateDelete = (el) => {
+    el.style.transform = "translateX(0px)";
+    el.style['-webkit-transform'] = "translateX(0px)";
+    let count = 0;
+    const animate = () => {
+      count -= 100;
+      if (count > -3000) {
+        el.style.transform = `translateX(${count}px)`;
+        el.style['-webkit-transform'] = `translateX(${count}px)`;
+        setTimeout(() => {
+          animate(el)
+        }, 10);
+      }
+    }
+    animate(el);
+  }
+
+  animateShow = (el) => {
+    el.style.transform = "translateX(-3000px)";
+    el.style['-webkit-transform'] = "translateX(-3000px)";
+    let count = -3000;
+    const animate = () => {
+      count += 100;
+      if (count < 0) {
+        el.style.transform = `translateX(${count}px)`;
+        el.style['-webkit-transform'] = `translateX(${count}px)`;
+        setTimeout(() => {
+          animate(el)
+        }, 10);
+      }
+    }
+    animate(el);
   }
 
   deleteItem() {
-    console.log(' this.todoData: ',  this.todoData);
+    const el = event.target.closest('li');
     this.todoData.forEach((item) => {
-      if (item.key === event.target.closest('li').key)  this.todoData.delete(item.key);      
-    })
-    this.render();
+      if (item.key === el.key) this.todoData.delete(item.key);
+    });
+    this.animateDelete(el);
+    setTimeout(() => {
+      this.render();
+    }, 300);
   }
 
   completedItem() {
+    let value, completed, key;
+    const el = event.target.closest('li');
     this.todoData.forEach((item) => {
-      if (item.key === event.target.closest('li').key) item.completed = true;
+      if (item.key === el.key) {
+        value = item.value;
+        console.log('value: ', value);
+        key = item.key;
+        console.log('key: ', key);
+        this.todoData.delete(item.key);
+      }
+    })
+    this.animateDelete(el);
+    setTimeout(() => {
+      this.render();
+    }, 300);
+
+    const newTodo = {
+      value: value,
+      completed: true,
+      key: key,
+    };
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+    li.key = newTodo.key;
+    li.insertAdjacentHTML('beforeend', `
+      <span class="text-todo">${newTodo.value}</span>
+      <div class="todo-buttons">
+        <button class="todo-edit"></button>
+        <button class="todo-remove"></button>
+        <button class="todo-complete"></button>
+      </div>
+      `);
+    li.style.transform = "translateX(-3000px)";
+    this.todoCompleted.append(li);
+    this.animateShow(li);
+    this.todoData.set(newTodo.key, newTodo);
+    setTimeout(() => {
+      this.render();
+    }, 300);
+  }
+
+  edit() {
+    const text = event.target.closest('.todo-item').querySelector('.text-todo');
+    text.setAttribute("contenteditable", "true");
+    text.addEventListener('blur', () => {
+      this.todoData.forEach((item) => {
+        console.log(text.closest('li'));
+        console.log('text.innerHTML: ', text.innerHTML);
+        console.log('item.value: ', item.value);
+        if (item.key === text.closest('li').key) item.value = text.innerHTML;
+      })
       this.render();
     })
+
   }
 
   handler() {
     document.querySelector('.todo-container').addEventListener('click', () => {
-      if (event.target.matches('.todo-complete')) this.completedItem()
-      else if (event.target.matches('.todo-remove')) this.deleteItem()
+      if (event.target.matches('.todo-complete')) this.completedItem();
+      else if (event.target.matches('.todo-remove')) this.deleteItem();
+      else if (event.target.matches('.todo-edit')) this.edit();
     })
   }
 
