588a280fc570695b4e5e4c398c0300501d60fbc8
Tatyana <soboleva.tv.spb@yandex.ru>
2020-07-16 18:40:09 +0400

    правки после второй проверки

--- src/modules/countTimer.js
+++ src/modules/countTimer.js
@@ -1,35 +1,52 @@
      const countTimer = (deadline) => {
-        const timerHours = document.querySelector('#timer-hours'),
-          timerMinutes = document.querySelector('#timer-minutes'),
-          timerSeconds = document.querySelector('#timer-seconds');
-        const getTimeRemaining = () => {
-          let dateStop = new Date(deadline).getTime(),
-            dateNow = new Date().getTime(),
-            timeRemaining = (dateStop - dateNow) / 1000,
-            seconds = Math.floor(timeRemaining % 60),
-            minutes = Math.floor((timeRemaining / 60) % 60),
-            hours = Math.floor(timeRemaining / 60 / 60);
-          return {
-            timeRemaining,
-            hours,
-            minutes,
-            seconds
-          };
-        }
-        const updateClock = () => {
-          let timer = getTimeRemaining();
-          if (timer.timeRemaining < 0) {
-            timerHours.textContent = '00';
-            timerMinutes.textContent = '00';
-            timerSeconds.textContent = '00';
-            clearInterval(updateClock, 1000);
-          } else {
-            timerHours.textContent = (timer.hours.toString().length == 2) ? (timer.hours) : ('0' + timer.hours);
-            timerMinutes.textContent = (timer.minutes.toString().length == 2) ? (timer.minutes) : ('0' + timer.minutes);
-            timerSeconds.textContent = (timer.seconds.toString().length == 2) ? (timer.seconds) : ('0' + timer.seconds);
-          }
-        }
-        setInterval(updateClock, 1000);
-      };
 
-      export default countTimer;
+       const timer = document.getElementById('timer');
+       timer.innerHTML = `
+          <span id="timer-hours"></span>
+          <span></span>
+          <span id="timer-minutes"></span>
+          <span></span>
+          <span id="timer-seconds"></span>          
+          `;
+       const timerHours = document.querySelector('#timer-hours'),
+         timerMinutes = document.querySelector('#timer-minutes'),
+         timerSeconds = document.querySelector('#timer-seconds'),
+         spanTimers = document.querySelectorAll('#timer>span');
+
+
+
+       const getTimeRemaining = () => {
+         let dateStop = new Date(deadline).getTime(),
+           dateNow = new Date().getTime(),
+           timeRemaining = (dateStop - dateNow) / 1000,
+           seconds = Math.floor(timeRemaining % 60),
+           minutes = Math.floor((timeRemaining / 60) % 60),
+           hours = Math.floor(timeRemaining / 60 / 60);
+         return {
+           timeRemaining,
+           hours,
+           minutes,
+           seconds
+         };
+       }
+       const updateClock = () => {
+         let timer = getTimeRemaining();
+         if (timer.timeRemaining < 0) {
+           timerHours.textContent = '00';
+           timerMinutes.textContent = '00';
+           timerSeconds.textContent = '00';
+           spanTimers[1].textContent=':';
+           spanTimers[3].textContent=':';
+           clearInterval(updateClock, 1000);
+         } else {
+           timerHours.textContent = (timer.hours.toString().length == 2) ? (timer.hours) : ('0' + timer.hours);
+           timerMinutes.textContent = (timer.minutes.toString().length == 2) ? (timer.minutes) : ('0' + timer.minutes);
+           timerSeconds.textContent = (timer.seconds.toString().length == 2) ? (timer.seconds) : ('0' + timer.seconds);
+           spanTimers[1].textContent=':';
+           spanTimers[3].textContent=':';
+         }
+       }
+       setInterval(updateClock, 1000);
+     };
+
+     export default countTimer;
--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,50 +1,56 @@
 const sendForm = (form) => {
-    const errorMessage = 'Что-то пошло не так';
-    const successMessage = 'Спасибо! Мы скоро с вами свяжемся';
-     
+  const errorMessage = 'Что-то пошло не так';
+  const successMessage = 'Спасибо! Мы скоро с вами свяжемся';
 
-    const statusMessage = document.createElement('div');
-    statusMessage.style.fontSize = '2rem';
-    statusMessage.style.color = '#ffffff';
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        statusMessage.innerHTML = `Загрузка...
+  const statusMessage = document.createElement('div');
+  statusMessage.style.fontSize = '2rem';
+  statusMessage.style.color = '#ffffff';
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    statusMessage.innerHTML = `Загрузка...
           <div class="sk-flow">
           <div class="sk-flow-dot"></div>
           <div class="sk-flow-dot"></div>
           <div class="sk-flow-dot"></div>
           </div>
           `
-        form.appendChild(statusMessage);
-        const formData = new FormData(form);
-        let body = {};
-        formData.forEach((val, key) => body[key] = val);     
+    form.appendChild(statusMessage);
+    const formData = new FormData(form);
+    let body = {};
+    formData.forEach((val, key) => body[key] = val);
 
-      const postData = (body) => {
-        return fetch('./server.php', {
-          method: 'POST',
-          headers: {'Content-Type': 'multipart/form-data'
-          },
-          body: JSON.stringify(body)
-        });
-      };
-
-      postData(body)
-      .then ((response) => {
-        if (response.status !== 200) {
-          throw new Error ('status error not 200');
-        }
-        statusMessage.textContent = successMessage;
-      })
-      .catch ((error) => {
-        statusMessage.textContent = errorMessage;
-          console.log(error);
-
-      })
-
-
-    })
+    const postData = (body) => {
+      return fetch('./server.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        body: JSON.stringify(body)
+      });
     };
 
-    export default sendForm;
+    const messageClear = () => {
+      statusMessage.textContent = '';
+    }
+
+    postData(body)
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error('status error not 200');
+        }
+        statusMessage.textContent = successMessage;
+        setTimeout (messageClear, 5000);
+      })
+      .catch((error) => {
+        statusMessage.textContent = errorMessage;
+        console.log(error);
+        setTimeout (messageClear, 5000);
+      })
+
+
+  })
+};
+
+export default sendForm;
