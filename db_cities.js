159e0cb0d5dea662ee958b5d9d05e10c8ce38de0
Tatyana <soboleva.tv.spb@yandex.ru>
2020-07-18 12:06:29 +0400

    part1

---
+++ db_cities.js
@@ -0,0 +1,458 @@
+const data = {
+    "RU": [{
+            "country": "Россия",
+            "count": "144500000",
+            "cities": [{
+                    "name": "Рязань",
+                    "count": "538962",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%A0%D1%8F%D0%B7%D0%B0%D0%BD%D1%8C"
+                },
+                {
+                    "name": "Москва",
+                    "count": "12615882",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0"
+                },
+                {
+                    "name": "Санкт-Петербург",
+                    "count": "5383968",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3"
+                },
+                {
+                    "name": "Краснодар",
+                    "count": "918145",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B4%D0%B0%D1%80"
+                },
+                {
+                    "name": "Екатеринбург",
+                    "count": "1484456",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3"
+                },
+                {
+                    "name": "Ростов-на-Дону",
+                    "count": "1130305",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%A0%D0%BE%D1%81%D1%82%D0%BE%D0%B2-%D0%BD%D0%B0-%D0%94%D0%BE%D0%BD%D1%83"
+                },
+                {
+                    "name": "Воронеж",
+                    "count": "1054537",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%92%D0%BE%D1%80%D0%BE%D0%BD%D0%B5%D0%B6"
+                }
+            ]
+
+        },
+        {
+            "country": "Германия",
+            "count": 82175684,
+            "cities": [{
+                    "name": "Берлин",
+                    "count": "3613495",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%91%D0%B5%D1%80%D0%BB%D0%B8%D0%BD"
+                },
+                {
+                    "name": "Мюнхен",
+                    "count": "1456039",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9C%D1%8E%D0%BD%D1%85%D0%B5%D0%BD"
+                },
+                {
+                    "name": "Франкфурт-на-Майне",
+                    "count": "736414",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%A4%D1%80%D0%B0%D0%BD%D0%BA%D1%84%D1%83%D1%80%D1%82-%D0%BD%D0%B0-%D0%9C%D0%B0%D0%B9%D0%BD%D0%B5"
+                },
+                {
+                    "name": "Кёльн",
+                    "count": "1080394",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9A%D1%91%D0%BB%D1%8C%D0%BD"
+                }
+            ]
+        },
+        {
+            "country": "Англия",
+            "count": 53012456,
+            "cities": [{
+                    "name": "Лондон",
+                    "count": " 8869898",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9B%D0%BE%D0%BD%D0%B4%D0%BE%D0%BD"
+                },
+                {
+                    "name": "Манчестер",
+                    "count": "545500",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9C%D0%B0%D0%BD%D1%87%D0%B5%D1%81%D1%82%D0%B5%D1%80"
+                },
+                {
+                    "name": "Эдинбург",
+                    "count": "488100",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%AD%D0%B4%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3"
+                },
+                {
+                    "name": "Бристоль",
+                    "count": "567111",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%91%D1%80%D0%B8%D1%81%D1%82%D0%BE%D0%BB%D1%8C"
+                }
+            ]
+
+        }
+    ],
+    "EN": [{
+            "country": "Russia",
+            "count": "144500000",
+            "cities": [{
+                    "name": "Ryazan",
+                    "count": "538962",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%A0%D1%8F%D0%B7%D0%B0%D0%BD%D1%8C"
+                },
+                {
+                    "name": "Moscow",
+                    "count": "12615882",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0"
+                },
+                {
+                    "name": "St Petersburg",
+                    "count": "5383968",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3"
+                },
+                {
+                    "name": "Krasnodar",
+                    "count": "918145",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B4%D0%B0%D1%80"
+                },
+                {
+                    "name": "Yekaterinburg",
+                    "count": "1484456",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3"
+                },
+                {
+                    "name": "Rostov-on-Don",
+                    "count": "1130305",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%A0%D0%BE%D1%81%D1%82%D0%BE%D0%B2-%D0%BD%D0%B0-%D0%94%D0%BE%D0%BD%D1%83"
+                },
+                {
+                    "name": "Voronezh",
+                    "count": "1054537",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%92%D0%BE%D1%80%D0%BE%D0%BD%D0%B5%D0%B6"
+                }
+            ]
+
+        },
+        {
+            "country": "Germany",
+            "count": 82175684,
+            "cities": [{
+                    "name": "Berlin",
+                    "count": "3613495",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%91%D0%B5%D1%80%D0%BB%D0%B8%D0%BD"
+                },
+                {
+                    "name": "Munich",
+                    "count": "1456039",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9C%D1%8E%D0%BD%D1%85%D0%B5%D0%BD"
+                },
+                {
+                    "name": "frankfurt",
+                    "count": "736414",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%A4%D1%80%D0%B0%D0%BD%D0%BA%D1%84%D1%83%D1%80%D1%82-%D0%BD%D0%B0-%D0%9C%D0%B0%D0%B9%D0%BD%D0%B5"
+                },
+                {
+                    "name": "Cologne",
+                    "count": "1080394",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9A%D1%91%D0%BB%D1%8C%D0%BD"
+                }
+            ]
+        },
+        {
+            "country": "United Kingdom",
+            "count": 53012456,
+            "cities": [{
+                    "name": "London",
+                    "count": " 8869898",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9B%D0%BE%D0%BD%D0%B4%D0%BE%D0%BD"
+                },
+                {
+                    "name": "Manchester",
+                    "count": "545500",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9C%D0%B0%D0%BD%D1%87%D0%B5%D1%81%D1%82%D0%B5%D1%80"
+                },
+                {
+                    "name": "Edinburgh",
+                    "count": "488100",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%AD%D0%B4%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3"
+                },
+                {
+                    "name": "Bristol",
+                    "count": "567111",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%91%D1%80%D0%B8%D1%81%D1%82%D0%BE%D0%BB%D1%8C"
+                }
+            ]
+
+        }
+    ],
+    "DE": [{
+            "country": "Russland",
+            "count": "144500000",
+            "cities": [{
+                    "name": "Ryazan",
+                    "count": "538962",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%A0%D1%8F%D0%B7%D0%B0%D0%BD%D1%8C"
+                },
+                {
+                    "name": "Moskau",
+                    "count": "12615882",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0"
+                },
+                {
+                    "name": "Saint Petersburg",
+                    "count": "5383968",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3"
+                },
+                {
+                    "name": "Krasnodar",
+                    "count": "918145",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B4%D0%B0%D1%80"
+                },
+                {
+                    "name": "Jekaterinburg",
+                    "count": "1484456",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3"
+                },
+                {
+                    "name": "Rostow",
+                    "count": "1130305",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%A0%D0%BE%D1%81%D1%82%D0%BE%D0%B2-%D0%BD%D0%B0-%D0%94%D0%BE%D0%BD%D1%83"
+                },
+                {
+                    "name": "Woronesch",
+                    "count": "1054537",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%92%D0%BE%D1%80%D0%BE%D0%BD%D0%B5%D0%B6"
+                }
+            ]
+
+        },
+        {
+            "country": "Deutschland",
+            "count": 82175684,
+            "cities": [{
+                    "name": "Berlin",
+                    "count": "3613495",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%91%D0%B5%D1%80%D0%BB%D0%B8%D0%BD"
+                },
+                {
+                    "name": "München",
+                    "count": "1456039",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9C%D1%8E%D0%BD%D1%85%D0%B5%D0%BD"
+                },
+                {
+                    "name": "Frankfurt",
+                    "count": "736414",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%A4%D1%80%D0%B0%D0%BD%D0%BA%D1%84%D1%83%D1%80%D1%82-%D0%BD%D0%B0-%D0%9C%D0%B0%D0%B9%D0%BD%D0%B5"
+                },
+                {
+                    "name": "Köln",
+                    "count": "1080394",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9A%D1%91%D0%BB%D1%8C%D0%BD"
+                }
+            ]
+        },
+        {
+            "country": "England",
+            "count": 53012456,
+            "cities": [{
+                    "name": "London",
+                    "count": " 8869898",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9B%D0%BE%D0%BD%D0%B4%D0%BE%D0%BD"
+                },
+                {
+                    "name": "Manchester",
+                    "count": "545500",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%9C%D0%B0%D0%BD%D1%87%D0%B5%D1%81%D1%82%D0%B5%D1%80"
+                },
+                {
+                    "name": "Edinburgh",
+                    "count": "488100",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%AD%D0%B4%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3"
+                },
+                {
+                    "name": "Bristol",
+                    "count": "567111",
+                    "link": "https://ru.wikipedia.org/wiki/%D0%91%D1%80%D0%B8%D1%81%D1%82%D0%BE%D0%BB%D1%8C"
+                }
+            ]
+        }
+    ]
+};
+
+const init = () => {
+    reset();
+    const listDefault = document.querySelector('.dropdown-lists__list--default');
+
+    listDefault.style.display = 'block';
+
+    const listDefaultDropDown = listDefault.querySelector('.dropdown-lists__col');
+
+    data['RU'].forEach((item) => {
+        const countryBlock = document.createElement('div');
+        countryBlock.classList.add('dropdown-lists__countryBlock');
+        countryBlock.innerHTML = `
+        <div class="dropdown-lists__total-line">
+        <div class="dropdown-lists__country">${item['country']}</div>
+        <div class="dropdown-lists__count">${item['count']}</div>
+        </div>`;
+        listDefaultDropDown.append(countryBlock);
+        const newCities = item['cities'] = item['cities'].sort((a, b) => b.count - a.count);
+
+        for (let i = 0; i < 3; i++) {
+            const cityLine = document.createElement('div');
+            cityLine.classList.add('dropdown-lists__line');
+            cityLine.innerHTML = `
+            <div class="dropdown-lists__city dropdown-lists__city--ip">${newCities[i]['name']}</div>
+            <div class="dropdown-lists__count">${newCities[i]['count']}</div>`;
+            listDefaultDropDown.append(cityLine);
+        }
+    })
+};
+
+const select = () => {
+        
+        inputValue(event.target.closest('.dropdown-lists__total-line').childNodes[1].textContent);
+
+        const listDefault = document.querySelector('.dropdown-lists__list--default');
+        listDefault.style.display = 'none';
+        const listSelect = document.querySelector('.dropdown-lists__list--select');
+        listSelect.style.display = 'block';
+        const listSelectDropDown = listSelect.querySelector('.dropdown-lists__col');
+
+        data['RU'].forEach((item) => {
+            if (item['country'] === event.target.closest('.dropdown-lists__total-line').childNodes[1].textContent) {
+                const countryBlock = document.createElement('div');
+                countryBlock.classList.add('dropdown-lists__countryBlock');
+                countryBlock.innerHTML = `
+                <div class="dropdown-lists__total-line">
+                <div class="dropdown-lists__country">${item['country']}</div>
+                <div class="dropdown-lists__count">${item['count']}</div>
+                </div>`;
+                    listSelectDropDown.append(countryBlock);
+
+                item['cities'].forEach((city) => {
+                    const cityLine = document.createElement('div');
+                    cityLine.classList.add('dropdown-lists__line');
+                    cityLine.innerHTML = `
+                <div class="dropdown-lists__city dropdown-lists__city--ip">${city['name']}</div>
+                <div class="dropdown-lists__count">${city['count']}</div>`;
+                    listSelectDropDown.append(cityLine);
+
+                })
+                listSelectDropDown.addEventListener('click', () => {
+                    if (event.target.closest('.dropdown-lists__total-line')) {
+                        inputValue(event.target.closest('.dropdown-lists__total-line').childNodes[1].textContent);
+                        listSelectDropDown.innerHTML = '';
+                        listSelect.style.display = 'none';
+                        listDefault.style.display = 'block';
+
+                    }
+                })
+            }
+        })
+    
+};
+
+const autocomplete = () => {
+    const listAutocomplete = document.querySelector('.dropdown-lists__list--autocomplete');
+    const listAutocompleteCountryBlock = listAutocomplete.querySelector('.dropdown-lists__countryBlock');
+    listAutocompleteCountryBlock.innerHTML = '';
+
+    const listDefault = document.querySelector('.dropdown-lists__list--default');
+    listDefault.style.display = 'none';
+
+    listAutocomplete.style.display = 'block';
+
+    data['RU'].forEach((item) => {
+        item['cities'].forEach((city) => {
+
+            if (city['name'].substr(0, input.value.length).toLowerCase() === input.value.toLowerCase()) {
+
+                const cityLine = document.createElement('div');
+                cityLine.classList.add('dropdown-lists__line');
+                cityLine.innerHTML = `
+                <div class="dropdown-lists__city dropdown-lists__city--ip">${city['name']}</div>
+                <div class="dropdown-lists__count">${city['count']}</div>`;
+                listAutocompleteCountryBlock.append(cityLine);
+            }
+        })
+    });
+
+    if (listAutocompleteCountryBlock.innerHTML === '') {
+        const cityLine = document.createElement('div');
+        cityLine.classList.add('dropdown-lists__line');
+        cityLine.innerHTML = `    
+        <div class="dropdown-lists__city dropdown-lists__city--ip">Ничего не найдено</div>`;
+        listAutocompleteCountryBlock.append(cityLine);
+
+    }
+
+    if (input.value === '') {
+        document.querySelector('.button').href = '#';   
+        reset();
+        listAutocomplete.style.display = 'none';
+        const listSelect = document.querySelector('.dropdown-lists__list--select');
+        listSelect.style.display = 'none';
+
+        const listDefault = document.querySelector('.dropdown-lists__list--default');
+        listDefault.style.display = 'block';
+    }
+};
+
+const inputValue = (value) => {
+    document.querySelector('.close-button').style.display = 'inline-block';
+    document.querySelector('.label').style.display = 'none';
+    input.value = value;
+    document.querySelector('.button').href = '#';
+
+    data['RU'].forEach((item) => {
+        item['cities'].forEach((city) => {
+            if (city['name'] === value) document.querySelector('.button').href = city['link'] 
+        });
+    }
+    )
+};
+
+const reset = () => {
+    const listDefault = document.querySelector('.dropdown-lists__list--default');
+    const listDefaultDropDown = listDefault.querySelector('.dropdown-lists__col');
+    listDefaultDropDown.innerHTML = '';
+
+    const listSelect = document.querySelector('.dropdown-lists__list--select');
+    const listSelectDropDown = listSelect.querySelector('.dropdown-lists__col');
+    listSelectDropDown.innerHTML = '';
+
+    const listAutocomplete = document.querySelector('.dropdown-lists__list--autocomplete');
+    const listAutocompleteCountryBlock = listAutocomplete.querySelector('.dropdown-lists__countryBlock');
+    listAutocompleteCountryBlock.innerHTML = '';
+};
+
+const input = document.getElementById('select-cities');
+input.addEventListener('keyup', () => autocomplete());
+
+document.addEventListener('click', (event) => {
+
+    if (event.target.closest('#select-cities')) init();
+
+    if (event.target.closest('.dropdown-lists__list--default') && event.target.closest('.dropdown-lists__col') && event.target.closest('.dropdown-lists__total-line')) select();
+
+    if (event.target.closest('dropdown-lists__country') || event.target.closest('.dropdown-lists__city')) inputValue(event.target.textContent);
+
+    if (event.target.closest('.close-button')) {
+        document.querySelector('.button').href = '#';        
+        event.target.style.display = 'none';
+        document.querySelector('.label').style.display = 'inline-block';
+        input.value = '';
+
+        reset();
+        const listAutocomplete = document.querySelector('.dropdown-lists__list--autocomplete');    
+        listAutocomplete.style.display = 'none';
+        
+        const listSelect = document.querySelector('.dropdown-lists__list--select');
+        listSelect.style.display = 'none';
+
+        const listDefault = document.querySelector('.dropdown-lists__list--default');
+        listDefault.style.display = 'block';
+       
+    }
+})
